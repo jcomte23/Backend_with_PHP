9f2b9dc26831c942b969deea5cca73bbf46aeede
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-03-05 17:36:59 -0500

    hello world program load

---
+++ 01_helloWorld/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Hello world! <br>";
+echo "This is a basic project created with PHP 8.2.0 and its only function is to print the message 'hello world'.";
+//este es un proyecto básico creado con PHP 8.2.0 y su única función es imprimir el mensaje 'hola mundo'.
