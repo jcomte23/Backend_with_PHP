7c04a8f4f9217c6627b73c8db1c5f323602af28f
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-03-16 23:17:01 -0500

    Validating data

---
+++ 06_Validating data/index.php
@@ -0,0 +1,25 @@
+<?php
+// Validar si es un número
+$isNumeric = is_numeric('3d'); // False
+$isNumeric1 = is_numeric('3'); // True
+$isNumeric2 = is_numeric(3); // True
+
+// Validar si es un string
+$isString = is_string('3d'); // True
+$isString1 = is_string('3'); // True
+$isString2 = is_string(3); // false
+
+// validar si es un array
+$isArray = is_array([]); // true
+$isArray2 = is_array(array()); // true
+$isArray3 = is_array('No soy un array xD soy un string'); // false
+
+//otras validaciones
+$is_float = filter_var("0.9", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_THOUSAND);
+$is_int = filter_var("45", FILTER_VALIDATE_INT);
+$is_ip = filter_var("127.0.0.1", FILTER_VALIDATE_IP);
+$is_url = filter_var("https://platzi.com", FILTER_VALIDATE_URL);
+$is_email = filter_var("mrmichi@michisancio.com", FILTER_VALIDATE_EMAIL);
+
+var_dump($is_float);
+?>
