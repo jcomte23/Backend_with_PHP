970a8303a56ecdff32d0e88805b7e3a1c457f17e
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-03-18 00:06:33 -0500

    carga de un proyecto php con nivel basico de composer

---
+++ 02_COMPOSER/01_nivelBasic/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+echo "To make the project run remember to run the 'composer dump' command inside the folder";
+echo "<br>";
+//Para que el proyecto corra recuerda correr el comando 'composer dump' dentro de la carpeta
+
+echo upper("lina cómbita");
+echo "<br>";
+echo lower("MARTINA PUMAREJO");
