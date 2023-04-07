2116a1d5307c69d20543606c9acf4a843efb1154
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-04-07 12:09:22 -0500

    mejoras en las cookies

--- 02_COMPOSER/06_Cookies/index.php
+++ 02_COMPOSER/06_Cookies/index.php
@@ -1,33 +1,24 @@
 <?php
 
+if (!isset($_COOKIE["exampleCookie"])) {
+
+    setcookie(
+        name: "exampleCookie",
+        value: "Cookie de ejemplo",
+        expires_or_options:0,//SS:MM:HH:DIAS o pon un zero para que se elimine cuando se cierre el navegador
+        path:"/",//la cookie estara disponible en todo el sitio web,o puedes espcificar la ruta:/compras
+        domain:"http://localhost:8000/",
+        secure:false,
+        //httponly:true,
+    );
+    echo "cookie definida";
+}
+
 setcookie(
     name: "header_color",
     value: "#000000"
 );
 
+
 $color = $_COOKIE["header_color"] ?? "#121f3d";
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Platzi</title>
-</head>
-
-<body>
-
-    <header style="
-            background: <?= $color ?>
-        ">
-        <img src="img/logo.png" alt="Platzi">
-    </header>
-
-</body>
-
-</html>
