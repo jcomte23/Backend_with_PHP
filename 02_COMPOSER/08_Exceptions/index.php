19e21d8f52c67c8ca310869ee9c0ec747833c391
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-04-07 13:31:30 -0500

    mejoras en el manejo de exceptions

--- 02_COMPOSER/08_Exceptions/index.php
+++ 02_COMPOSER/08_Exceptions/index.php
@@ -1,10 +1,20 @@
 <?php
 
 try {
-    $resultado = 20 / 2;
+    $resultado = 20 / 0;
     echo $resultado;
 } catch (Throwable $e) {
     echo $e->getMessage();
+    echo "<br>";
+    echo $e->getCode();
+    echo "<br>";
+    echo $e->getFile();
+    echo "<br>";
+    echo $e->getLine();
+    echo "<br>";
+    echo $e->getTrace();
+    echo "<br>";
+    echo $e->getTraceAsString();
     echo "<br>";
     echo "¡Ups! Algo salió mal con tu división";
 }
@@ -19,15 +29,13 @@ echo "Ejercicio2<br>";
 try {
 
     $pet = readline("¿Qué quieres adoptar? ");
-    $pet="conejo";
+    $pet = "conejo";
 
     if ($pet != "michi" && $pet != "conejo")
         throw new Exception("Lo sentimos, no tenemos $pet en este centro de adopción");
 
     echo "¡Felicidades por tu nuevo $pet!";
-    
 } catch (\Throwable $e) {
 
     echo $e->getMessage();
-
 }
